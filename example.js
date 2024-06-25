7a1f28d33f9e7f70f5e536f817b41241c5f59dba
joshtrinh1102 <nltrinh1102@gmail.com>
2024-06-24 21:28:30 -0400

    Create dynamic stock visualization using D3.js

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 function main(){
-    d3.dsv(",",'../data/mock_stock_data(1).csv').then(
+    d3.dsv(",","../data/mock_stock_data (1).csv").then(
         function (data) {
             for (let index = 0; index <d.length; index++){
                 const element = d[index];
@@ -11,5 +11,45 @@ function main(){
 ).catch(function(error){
         console.log(error);
     });
-    }
+//Parse and filter csv data
+const svgWidth = 600;
+const svgHeight= 600;
+const barHeight= 20;
+const barMargin=1;
+const svg = d3.select("body").append("svg")
+    .attr("width",svgWidth)
+    .attr("height",svgHeight);
+
+const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, svgWidth -60])
     
+svg.append("g")
+    .attr("transform", `translate(50,)`)
+    .call(d3.axisBottom(xScale))
+
+svg.selectAll("g.bar")
+    .data(data)
+    .enter().append("g")
+    .attr("transform", (d,i) => `translate(60, ${i * (barHeight + barMargin)})`)
+    .each(function(d,i){
+        const group = d3.select(this);
+
+        group.append("rect")
+        .attr("width",0)
+        .attr("height", barHeight - barMargin)
+        .attr("fill","firebrick")
+        .transition()
+        .duration(1000)
+        .attr("width", xScale(d));
+
+        group.append("text")
+        .attr("x", xScale(d) +3)
+        .attr("y", barheight /2)
+        .attr("dy" , ".35em")
+        .text (d)
+    });
+
+ 
+    
+
